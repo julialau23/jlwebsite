66ff8bb00b03be32e6f53819487e9e4f23c5c2d2
Julia Lau <julia800@gmail.com>
2020-01-29 21:50:25 -0800

    updated header/styling, added grid layout to css

--- app.js
+++ app.js
@@ -1,8 +1,58 @@
-'use strict'
+// 'use strict'
 
-console.log('Welcome to my site!')
+/* <script type="text/javascript"></script> */
+/* <script type="text/javascript" src="http://code.jquery.com/jquery-1.7.1.min.js"></script>
+    <script src="/lib/jquery.min.js"></script>
+    <script src="/lib/jquery.plugin.js"></script> */
 
-let today = new Date()
-let formatDate = today.toDateString()
-let selectElement = document.getElementById('date')
-selectElement.innerHTML = formatDate
+    // <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+$(window).scroll(function() {
+    var scrollTop = $(window).scrollTop();
+    var imgPos = scrollTop / 2 + 'px';
+    $('.main-head-img').css('transform', 'translateY(' + imgPos + ')');
+});
+
+
+// $(function(){  // $(document).ready shorthand
+//     $('.main-head').fadeIn('slow');
+//   });
+  
+//   $(document).ready(function() {
+      
+//       /* Every time the window is scrolled ... */
+//       $(window).scroll( function(){
+      
+//           /* Check the location of each desired element */
+
+              
+//             var bottom_of_object = $('.item2').position().top + $('.item2').outerHeight();
+//             var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+//             /* If the object is completely visible in the window, fade it it */
+//             if( bottom_of_window > bottom_of_object ){
+                
+//                 $('.item2').animate({'opacity':'1'},1500);
+             
+//             }
+//             });
+            
+
+
+//         //   $('item2').each( function(){
+              
+//         //       var bottom_of_object = $(this).position().top + $(this).outerHeight();
+//         //       var bottom_of_window = $(window).scrollTop() + $(window).height();
+              
+//         //       /* If the object is completely visible in the window, fade it it */
+//         //       if( bottom_of_window > bottom_of_object ){
+                  
+//         //           $(this).animate({'opacity':'1'},1500);
+                      
+//         //       }
+              
+//         //   }); 
+      
+//     //   });
+      
+//   });

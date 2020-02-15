ab3fc0ea3f6dc016a03925524f90d96d32909077
Julia Lau <julia800@gmail.com>
2020-02-15 10:55:58 -0800

    changed from grid to flex-box, made nav-bar and body responsive

--- app.js
+++ app.js
@@ -7,51 +7,116 @@
 
     // <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
-$(window).scroll(function() {
-    var scrollTop = $(window).scrollTop();
-    var imgPos = scrollTop / 2 + 'px';
-    $('.main-head-img').css('transform', 'translateY(' + imgPos + ')');
-});
+// $(window).scroll(function() {
+//     var scrollTop = $(window).scrollTop();
+//     var imgPos = scrollTop / 2 + 'px';
+//     $('.main-head-img').css('transform', 'translateY(' + imgPos + ')');
+// });
 
-
-// $(function(){  // $(document).ready shorthand
-//     $('.main-head').fadeIn('slow');
-//   });
+// function openNav() {
+//     document.getElementById("topNav").style.width = "25%";
+//   }
   
-//   $(document).ready(function() {
-      
-//       /* Every time the window is scrolled ... */
-//       $(window).scroll( function(){
-      
-//           /* Check the location of each desired element */
+//   function closeNav() {
+//     document.getElementById("topNav").style.width = "100%";
+//   }
 
-              
-//             var bottom_of_object = $('.item2').position().top + $('.item2').outerHeight();
-//             var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
-//             /* If the object is completely visible in the window, fade it it */
-//             if( bottom_of_window > bottom_of_object ){
-                
-//                 $('.item2').animate({'opacity':'1'},1500);
-             
+
+function openNav() {
+    var x = document.getElementById("nav");
+    if (x.className === "stroke") {
+      x.className += " responsive";
+    } else {
+      x.className = "stroke";
+    }
+  }
+
+
+
+// (function(window, document, undefined)
+// {
+
+//     window.App = (function()
+//     {
+
+//         var _init = false, app = { };
+
+//         app.init = function()
+//         {
+//             if (_init) {
+//                 return;
 //             }
+//             _init = true;
+
+//             var nav_open = false,
+//                 $main = $('.main-head');
+
+//             $('#nav-open-btn').on('click', function()
+//             {
+//                 if (!nav_open) {
+//                     $main.animate({ left: '70%' }, 500);
+//                     nav_open = true;
+//                     return false;
+//                 }
 //             });
+
+//             $('#nav-close-btn').on('click', function()
+//             {
+//                 if (nav_open) {
+//                     $main.animate({ left: '0' }, 500);
+//                     nav_open = false;
+//                     return false;
+//                 }
+//             });
+
+//             $(document.documentElement).addClass('js-ready');
+//         };
+
+//         return app;
+
+//     })();
+
+//     $.fn.ready(function()
+//     {
+//         window.App.init();
+//     });
+
+// })(window, window.document);
+
+
+
+$(function(){  // $(document).ready shorthand
+    $(".header-container").fadeIn('slow');
+  });
+  
+  $(document).ready(function() {
+      
+      /* Every time the window is scrolled ... */
+      $(window).scroll( function(){
+      
+          /* Check the location of each desired element */
+
+          $(".main-container").children().each( function(i){
+            
+            var middle_of_object = $(this).position().top + $(this).outerHeight()/3;
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            // console.log($(this).outerHeight());
+            /* If the object is completely visible in the window, fade it in */
+            if( bottom_of_window > middle_of_object ){
+                
+                $(this).animate({'opacity':'1'},1300);
+                    
+            }
+            
+        }); 
+            
+    });
+        });
+
             
 
 
-//         //   $('item2').each( function(){
-              
-//         //       var bottom_of_object = $(this).position().top + $(this).outerHeight();
-//         //       var bottom_of_window = $(window).scrollTop() + $(window).height();
-              
-//         //       /* If the object is completely visible in the window, fade it it */
-//         //       if( bottom_of_window > bottom_of_object ){
-                  
-//         //           $(this).animate({'opacity':'1'},1500);
-                      
-//         //       }
-              
-//         //   }); 
+
       
 //     //   });
       
